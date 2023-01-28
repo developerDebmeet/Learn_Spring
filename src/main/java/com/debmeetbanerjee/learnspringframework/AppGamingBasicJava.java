a5997afefff03574d97d3126d18744176ddb336b
debmeetDev <debmeet.banerjee@gmail.com>
2023-01-28 21:38:59 +0530

    .

--- src/main/java/com/debmeetbanerjee/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/com/debmeetbanerjee/learnspringframework/AppGamingBasicJava.java
@@ -6,8 +6,9 @@ import com.debmeetbanerjee.learnspringframework.game.MarioGame;
 public class AppGamingBasicJava {
     public static void main(String[] args) {
 
-        var marioGame = new MarioGame();
-        var gameRunner = new GameRunner(marioGame);
+        //var marioGame = new MarioGame();
+        var superContraGame = new SuperContraGame();
+        var gameRunner = new GameRunner(superContraGame);
         gameRunner.run();
 
     }
