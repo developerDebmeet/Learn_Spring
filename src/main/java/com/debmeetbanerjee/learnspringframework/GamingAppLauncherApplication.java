3656c4489f1e16720366f8aacad9a63530856c1c
debmeetAsus <debmeetbanerjee@gmail.com>
2023-01-29 16:45:07 +0530

    .

--- src/main/java/com/debmeetbanerjee/learnspringframework/GamingAppLauncherApplication.java
+++ src/main/java/com/debmeetbanerjee/learnspringframework/GamingAppLauncherApplication.java
@@ -3,11 +3,16 @@ package com.debmeetbanerjee.learnspringframework;
 import com.debmeetbanerjee.learnspringframework.game.GameRunner;
 import com.debmeetbanerjee.learnspringframework.game.GamingConsole;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-public class App03GamingSpringBeans {
+@Configuration
+@ComponentScan("com.debmeetbanerjee.learnspringframework.game")
+public class GamingAppLauncherApplication {
+
     public static void main(String[] args) {
 
-        try (var context = new AnnotationConfigApplicationContext(App03Configuration.class)) {
+        try (var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class)) {
             context.getBean(GamingConsole.class).up();
             context.getBean(GameRunner.class).run();
         }
--- src/main/java/com/debmeetbanerjee/learnspringframework/game/PacmanGame.java
+++ src/main/java/com/debmeetbanerjee/learnspringframework/game/PacmanGame.java
@@ -1,5 +1,10 @@
 package com.debmeetbanerjee.learnspringframework.game;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component // Instance of Packman Game will be made by Spring automatically
+@Primary
 public class PacmanGame implements GamingConsole{
 
     public void up() {
